37f4397438cd7228ca0b43075d9c910c76bf03cb
Edyta Bednarz <edy.b.bednarz@gmail.com>
2020-05-25 19:28:00 +0100

    Fixed issue with the menu, can choose another file to play

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,12 +4,12 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.Menu;
 
 import java.io.File;
 import java.net.MalformedURLException;
@@ -17,44 +17,44 @@ import java.net.MalformedURLException;
 
 public class Main extends Application {
 
-	 Player player;
-	 FileChooser fileChooser;
+	Player player;
+	FileChooser fileChooser;
 
 	public void start(final Stage primaryStage) {
+		MenuItem open = new MenuItem("Open");
+		Menu file = new Menu("File");
+		MenuBar menu = new MenuBar();
 
-      MenuItem open = new MenuItem("Open");
-      Menu file = new Menu("File");
-      MenuBar menu = new MenuBar();
+		file.getItems().add(open);
+		menu.getMenus().add(file);
 
-      file.getItems().add(open);
-      menu.getMenus().add(file);
+		fileChooser = new FileChooser();
 
-      fileChooser = new FileChooser();
+		open.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent event) {
+				player.player.pause();
+				File file = fileChooser.showOpenDialog(primaryStage);
+				if (file != null) {
+					try {
+						player = new Player(file.toURI().toURL().toExternalForm());
+						Scene scene = new Scene(player, 720, 510, Color.BLACK);
+						primaryStage.setScene(scene);
+					} catch (MalformedURLException e) {
+						e.printStackTrace();
+						}
+				}
+			}
+		});
 
-      open.setOnAction(new EventHandler<ActionEvent>() {
-	      	public void handle(ActionEvent event) {
-		      player.player.pause();
-		      File file = fileChooser.showOpenDialog(primaryStage);
-		      if(file != null){
-		      	try {
-		      		player = new Player(file.toURI().toURL().toExternalForm());
-		      		Scene scene = new Scene(player, 720, 535, Color.BLACK);
-		      		primaryStage.setScene(scene );
-		        } catch (MalformedURLException e){
-		      		e.printStackTrace();
-		        }
-		      }
-	      }
-      });
+		player = new Player("file:/Users/edytabednarz/Developer/Video.mp4");
+		player.setTop(menu);
+		Scene scene = new Scene(player, 720, 510, Color.BLACK);
 
-      Player player = new Player("file:/Users/edytabednarz/Developer/Video.mp4");
-      Scene scene = new Scene(player, 720, 480, Color.BLACK);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
 
-      primaryStage.setScene(scene);
-      primaryStage.show();
-  }
-
-    public static void main(String[] args) {
-        launch(args);
-    }
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
