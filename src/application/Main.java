935fd54cc714f0989607b361540cdeba6c7491bb
Edyta Bednarz <edy.b.bednarz@gmail.com>
2020-05-25 17:43:01 +0100

    Adding file choser in menu bar --WIP

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,17 +1,55 @@
 package application;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.Menu;
+
+import java.io.File;
+import java.net.MalformedURLException;
 
 
 public class Main extends Application {
 
-  @Override
-  public void  start(Stage primaryStage) {
+	 Player player;
+	 FileChooser fileChooser;
+
+	public void start(final Stage primaryStage) {
+
+      MenuItem open = new MenuItem("Open");
+      Menu file = new Menu("File");
+      MenuBar menu = new MenuBar();
+
+      file.getItems().add(open);
+      menu.getMenus().add(file);
+
+      fileChooser = new FileChooser();
+
+      open.setOnAction(new EventHandler<ActionEvent>() {
+	      	public void handle(ActionEvent event) {
+		      player.player.pause();
+		      File file = fileChooser.showOpenDialog(primaryStage);
+		      if(file != null){
+		      	try {
+		      		player = new Player(file.toURI().toURL().toExternalForm());
+		      		Scene scene = new Scene(player, 720, 535, Color.BLACK);
+		      		primaryStage.setScene(scene );
+		        } catch (MalformedURLException e){
+		      		e.printStackTrace();
+		        }
+		      }
+	      }
+      });
+
       Player player = new Player("file:/Users/edytabednarz/Developer/Video.mp4");
       Scene scene = new Scene(player, 720, 480, Color.BLACK);
+
       primaryStage.setScene(scene);
       primaryStage.show();
   }
